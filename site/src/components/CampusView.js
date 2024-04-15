bd94ab1e19178df9cd12037da93abc710aba747a
alex-oh <oh.alexj@gmail.com>
2024-04-15 02:01:24 -0400

    Add other component links to sidebar

---
+++ site/src/components/CampusView.js
@@ -0,0 +1,9 @@
+function CampusView() {
+    return (
+        <div>
+            <h1>Campus View</h1>
+            <p>Display metrics of each campus</p>
+        </div>
+    );
+}
+export default CampusView;
--- site/src/components/MainDash.js
+++
@@ -1,9 +0,0 @@
-function MainDash() {
-    return(
-        <div>
-            <h1>Main Dashboard</h1>
-        </div>
-    );
-}
-
-export default MainDash;
