af1625b2784e2e1ff4696cb31f1d2b3eda590bee
alex-oh <oh.alexj@gmail.com>
2024-04-15 02:51:51 -0400

    Add chart js library

--- site/src/components/InvoiceView.js
+++ site/src/components/InvoiceView.js
@@ -1,7 +1,10 @@
 function InvoiceView() {
+    // Possible feature: link to detailed invoice view via /invoices/<invoice number>
+    // for every single invoice listed in table
     return (
         <div>
             <h1>Invoice View</h1>
+            <p>Table here with invoice properties</p>
         </div>
     );
 }
