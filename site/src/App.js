569b3761d4d9dd95902e1a9469062a0ffb1c7710
alex-oh <oh.alexj@gmail.com>
2024-04-15 21:53:26 -0400

    Remove unused imports

--- site/src/App.js
+++ site/src/App.js
@@ -1,11 +1,8 @@
-import logo from "./logo.svg";
 import "./App.css";
 import Nav from "./components/Nav";
 import Dashboard from "./Dashboard";
 
 import { HashRouter } from "react-router-dom";
-import { Routes, Route } from "react-router";
-import { Navigate } from "react-router-dom";
 
 function App() {
     return (
